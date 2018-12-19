9d654b0144e97fbcc7e176e87aeb3ea1fd4b4b40
Diego Castillo <diegocasmo@gmail.com>
2018-12-20 00:07:41 +0100

    Use CNN to classify CAPTCHAs

--- lab-5/myclassifier.m
+++ lab-5/myclassifier.m
@@ -1,13 +1,19 @@
-function S = myclassifier(im)
+function S = myclassifier(I,trained_net)
+  % Setup categorical label to label mapping
+  labels = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'};
+  categories = {[0, 0, 0] [0,0,1] [0,0,2] [0,1,0] [0,1,1] [0,1,2] [0,2,0] [0,2,1] [0,2,2] [1,0,0] [1,0,1] [1,0,2] [1,1,0] [1,1,1] [1,1,2] [1,2,0] [1,2,1] [1,2,2] [2,0,0] [2,0,1] [2,0,2] [2,1,0] [2,1,1] [2,1,2] [2,2,0] [2,2,1] [2,2,2]};
+  categorical_to_label = containers.Map(labels,categories);
 
-%This classifier is not state of the art... but should give you an idea of
-%the format we expect to make it easy to keep track of your scores. Input
-%is the image, output is a 1 x 3 vector of the three numbers in the image
-%
-%This baseline classifier tries to guess... so should score about (3^3)^-1
-%on average, approx. a 4% chance of guessing the correct answer. 
-%
+  % Pre-process input image
+  I = medfilt2(I,[8 8]);
+  I = imcrop(I,[65 52 139 99]);
+  I = imbinarize(I,graythresh(I));
+  I = uint8(I);
 
-S = floor(rand(1,3)*3);
+  % Classify image
+  categorical_label = classify(trained_net,I);
+
+  % Get label given categorical label
+  S = categorical_to_label(char(categorical_label));
 end
 
